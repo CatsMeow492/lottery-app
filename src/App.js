11ca0b46645ec37b514ac8f7f3b335d447985b27
CatsMeow492 <calisurf243@hotmail.com>
2022-04-17 09:27:30 -0700

    updated form

--- src/App.js
+++ src/App.js
@@ -3,25 +3,42 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { manager: "" };
-    this.state = { user: "" };
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: ""
   }
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-    const user = await web3.eth.getAccounts();
-    this.setState({ manager });
-    this.setState({ user });
+    const players = await web3.eth.getPlayers().call;
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>Wallet connected {this.state.user}</p>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+        Wallet connected {this.state.user}
+        This contract is managed by {this.state.manager}
+        There are concurrently {this.state.players.length} people entered 
+        competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        <hr />
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+          </form>
       </div>
     );
   }
